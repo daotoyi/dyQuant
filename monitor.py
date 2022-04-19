3a8886a0b4d443e86b3d735f7323bbdc6bd95282
daotoyi <wenhuas.shi@gmail.com>
2022-04-19 15:30:51 +0800

    add WechatPub send message

--- monitor.py
+++ monitor.py
@@ -175,6 +175,48 @@ class WeChat():
         win32clipboard.CloseClipboard()
 
 
+class WeChatPub():
+    s = requests.session()
+    CORP_ID = "xxxx"
+    SECRET = "xxxx"
+
+    def __init__(self):
+        self.token = self.get_token()
+
+    def get_token(self):
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={CORP_ID}&corpsecret={SECRET}"
+        rep = self.s.get(url)
+        if rep.status_code != 200:
+            print("request failed.")
+            return
+        return json.loads(rep.content)['access_token']
+
+    def send_msg(self, content):
+        url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" + self.token
+        header = {
+            "Content-Type": "application/json"
+        }
+        form_data = {
+            "touser": "FengXianMei",
+            "toparty": "1",
+            "totag": " TagID1 | TagID2 ",
+            "msgtype": "textcard",
+            "agentid": 1000002, #Application ID
+            "textcard": {
+                "title": "Message Titile",
+                "description": content,
+                "url": "URL",
+                "btntxt": "More"
+            },
+            "safe": 0
+        }
+        rep = self.s.post(url, data=json.dumps(form_data).encode('utf-8'), headers=header)
+        if rep.status_code != 200:
+            print("request failed.")
+            return
+        return json.loads(rep.content)
+
+
 class QQ():
     def send(self, userName, msg):
 
